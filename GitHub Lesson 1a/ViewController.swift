ee1bda073b69a6c94f6b78fccf57a12063e8c425
patrick sebastian lie <patricksebastian020497@gmail.com>
2019-05-15 08:14:07 +0700

    Initial Commit

---
+++ GitHub Lesson 1a/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Lesson 1a
+//
+//  Created by Patrick Sebastian Lie on 15/05/19.
+//  Copyright © 2019 Patrick Sebastian Lie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
