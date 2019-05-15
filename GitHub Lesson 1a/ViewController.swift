bd5964cc8192bcc83b5357d71ff7751740c5747f
patrick sebastian lie <patricksebastian020497@gmail.com>
2019-05-15 08:28:21 +0700

    add SECOND print function to view controller

--- GitHub Lesson 1a/ViewController.swift
+++ GitHub Lesson 1a/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is Da Wae")
+        print("That is not Da Wae")
     }
 
 
